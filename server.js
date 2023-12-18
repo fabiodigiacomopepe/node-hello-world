5d3f765e07c96d673c589acced2e766e57a8506e
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-12-18 22:52:00 +0100

    add bonus

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const http = require("http");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
+const frasi = [
+    "frase 1",
+    "frase 2",
+    "frase 3",
+    "frase 4"
+]
+
+function htmlResponse(res, content) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-})
-    .listen(port, host, () => {
-        const serverUrl = `http://localhost:${port}`
-        console.log(`Server avviato su ${serverUrl}`);
-    });
+    res.end(content);
+}
+
+function numeroCasuale() {
+    return Math.floor(Math.random() * 4);
+}
+
+const server = http.createServer(function (req, res) {
+    htmlResponse(res,
+        `Hello World! <br>
+        ${process.env.TEXT} <br>
+        <h1>${frasi[numeroCasuale()]}</h1>`
+    );
+});
+
+server.listen(port, () => {
+    const serverUrl = `http://localhost:${port}`
+    console.log(`Server avviato su ${serverUrl}`);
+});
